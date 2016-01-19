22e754e9f12291a32f5145cb60f3207e365fc45e
Darya <darya.isakova.952gmail.com>
2016-01-19 17:01:27 +0300

    first commit

---
+++ fs.c
@@ -0,0 +1,147 @@
+#define FUSE_USE_VERSION 26
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+/* For pread()/pwrite() */
+#define _XOPEN_SOURCE 500
+#endif
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+#define PATHLEN_MAX 1024
+
+
+
+static int fs_getattr(const char *path, struct stat *stbuf);
+static int fs_mkdir(const char *path, mode_t mode);
+static int fs_rmdir(const char *path);
+static int fs_truncate(const char *path, off_t size);
+static int fs_open(const char *path, struct fuse_file_info *fi);
+static int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
+static int fs_read(const char *path, char *buf, size_t size, off_t offset,  struct fuse_file_info *fi);
+
+
+static int fs_getattr(const char *path, struct stat *stbuf)
+{
+	int res;
+
+	res = lstat(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int fs_mkdir(const char *path, mode_t mode)
+{
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int fs_rmdir(const char *path)
+{
+	int res;
+
+	res = rmdir(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+
+
+static int fs_truncate(const char *path, off_t size)
+{
+	int res;
+
+	res = truncate(path, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int fs_open(const char *path, struct fuse_file_info *fi)
+{
+	int res;
+
+	res = open(path, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	close(res);
+	return 0;
+}
+
+static int fs_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int fs_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static struct fuse_operations oper = {
+	.getattr	= fs_getattr,
+	.read	= fs_read,
+	.write	=fs_write,
+	.open	=fs_open,
+	.mkdir		= fs_mkdir,
+	.rmdir		= fs_rmdir,
+	.truncate	=fs_truncate
+};
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+    return fuse_main(argc, argv, &oper, NULL);
+}
